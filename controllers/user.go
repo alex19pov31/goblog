29a6703d675aba48c37b79abaa9419cd579d4d5e
Nesterov Alexander <nesterov@greensight.tu>
2017-05-04 02:26:35 +0300

    update user controller

--- controllers/user.go
+++ controllers/user.go
@@ -39,50 +39,63 @@ func (pc *UserController) Create(rt *helpers.Route) {
 	rt.Request.ParseForm()
 	data := rt.Request.Form
 
-	Post := models.Post{
-		Title:      data.Get("title"),
-		Preview:    data.Get("preview"),
-		Text:       data.Get("text"),
+	User := models.User{
+		Id:         bson.NewObjectId(),
+		Login:      data.Get("login"),
+		Email:      data.Get("email"),
+		Role:       data.Get("role"),
+		Password:   data.Get("password1"),
+		Active:     data.Get("active"),
 		Created_at: time.Now(),
 		Updated_at: time.Now()}
-	ur.Collection.Insert(&Post)
-	rt.Data["Post"] = Post
+	ur.Collection.Insert(&User)
+	rt.Data["User"] = User
 
 	if data.Get("action") == "save" {
-		rt.Redirect("/admin/posts/", 302)
+		rt.Redirect("/admin/users/"+User.Id.Hex(), 302)
 	} else {
-		rt.Render("layout", false, "view/admin/layout.html", "view/admin/post.html")
+		rt.Render("layout", false, "view/admin/layout.html", "view/admin/user.html")
 	}
 }
 
 func (pc *UserController) Update(rt *helpers.Route) {
-	id := regexp.MustCompile("/posts/([^/\\?]{24})/?(|\\?.*)$").FindStringSubmatch(rt.Request.RequestURI)[1]
+	id := regexp.MustCompile("/users/([^/\\?]{24})/?(|\\?.*)$").FindStringSubmatch(rt.Request.RequestURI)[1]
 
 	rt.Request.ParseForm()
 	data := rt.Request.Form
-	Post := models.Post{
-		Title:      data.Get("title"),
-		Preview:    data.Get("preview"),
-		Text:       data.Get("text"),
-		Updated_at: time.Now()}
-	ur.Collection.Update(bson.M{"_id": bson.ObjectIdHex(id)}, &Post)
-	rt.Data["Post"] = Post
+	Post, err := ps.FindOne(bson.M{"_id": bson.ObjectIdHex(id)})
+	if err == nil {
+		User.Login = data.Get("login")
+		User.Email = data.Get("email")
+		User.Role = data.Get("role")
+		User.Password = data.Get("password1")
+		User.Active = data.Get("active")
+		User.Updated_at = time.Now()
+		ur.Collection.Update(bson.M{"_id": bson.ObjectIdHex(id)}, &User)
+	}
+	rt.Data["User"] = User
 
 	if data.Get("action") == "save" {
-		rt.Redirect("/admin/posts/", 302)
+		rt.Redirect("/admin/users/", 302)
 	} else {
-		rt.Render("layout", false, "view/admin/layout.html", "view/admin/post.html")
+		rt.Render("layout", false, "view/admin/layout.html", "view/admin/user.html")
 	}
 }
 
 func (pc *UserController) Delete(rt *helpers.Route) {
-	id := regexp.MustCompile("/posts/delete/([^/\\?]{24})/?(|\\?.*)$").FindStringSubmatch(rt.Request.RequestURI)[1]
+	id := regexp.MustCompile("/users/delete/([^/\\?]{24})/?(|\\?.*)$").FindStringSubmatch(rt.Request.RequestURI)[1]
 	ur.Collection.Remove(bson.M{"_id": bson.ObjectIdHex(id)})
-	rt.Redirect("/admin/posts/", 302)
+	rt.Redirect("/admin/users/", 302)
 }
 
 func (pc *UserController) Active(rt *helpers.Route) {
-	id := regexp.MustCompile("/posts/delete/([^/\\?]{24})/?(|\\?.*)$").FindStringSubmatch(rt.Request.RequestURI)[1]
+	id := regexp.MustCompile("/posts/users/([^/\\?]{24})/?(|\\?.*)$").FindStringSubmatch(rt.Request.RequestURI)[1]
+	Post, err := ps.FindOne(bson.M{"_id": bson.ObjectIdHex(id)})
+	if err != nil {
+		Post.Active = true
+		User.Updated_at = time.Now()
+		ur.Collection.Update(bson.M{"_id": bson.ObjectIdHex(id)}, &User)
+	}
 	ur.Collection.Remove(bson.M{"_id": bson.ObjectIdHex(id)})
 	rt.Redirect("/admin/posts/", 302)
 }
